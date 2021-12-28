3ba72a44f109272525cb492346444ea1788cddf2
WalterOkumu <okumu.oriaro@gmail.com>
2021-12-28 13:00:34 +0300

    fixed coinranking issue with local backend

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,23 +1,30 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const cryptoApiHeaders = {
-  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": "8fc9800f7cmsh24a5ca2d9558d6ep1537c4jsn3a3138fd2d05"
-};
+// const cryptoApiHeaders = {
+//   'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//   'x-rapidapi-key': '8fc9800f7cmsh24a5ca2d9558d6ep1537c4jsn3a3138fd2d05'
+// }
 
-const baseUrl = "https://coinranking1.p.rapidapi.com";
+// const cryptoApiHeaders = {
+//   'x-access-token': 'coinranking15bb086be6719a35c4dfbfcf019332654ec3a47a7b0fb028'
+// }
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+//const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const baseUrl = `http://localhost:8000`
+
+//const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+const createRequest = (url) => ({ url })
 
 export const cryptoApi = createApi({
-  reducerPath: "cryptoApi",
+  reducerPath: 'cryptoApi',
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest("/coins")
+      query: () => createRequest('/coins')
     })
   })
-});
+})
 
-export const { useGetCryptosQuery } = cryptoApi;
-
+export const { useGetCryptosQuery } = cryptoApi
